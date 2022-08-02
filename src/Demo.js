c8e9858d32b0255cbb5701fc817d8476fd41913a
GiaNinh <80795610+GiaNinh@users.noreply.github.com>
2022-08-03 00:07:02 +0700

    Update Demo.js

--- src/Demo.js
+++ src/Demo.js
@@ -11,7 +11,7 @@ export default function Demo() {
   const [turn, setTurn] = useState(true); // turn{true,false} true là lượt của human, false là lượt ai
 
   //handleClick: lượt của human
-  
+
   const handleClick = i => {
     if (checkWinner(squares) || squares[i] !== null) {
       return;
@@ -21,54 +21,53 @@ export default function Demo() {
     setTurn(!turn);
     // check win
     if (checkWinner(squares) === null) {
-      
       bestMove(turn, squares);
     }
   };
 
-    //checkWinner
+  //checkWinner
 
   function checkWinner(squares) {
-	  const lines = [
-	    [0, 1, 2],
-	    [3, 4, 5],
-	    [6, 7, 8],
-	    [0, 3, 6],
-	    [1, 4, 7],
-	    [2, 5, 8],
-	    [0, 4, 8],
-	    [2, 4, 6]
-	  ];
-	  for (let i = 0; i < lines.length; i++) {
-	    const [a, b, c] = lines[i];
-	    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-		    return squares[a];
-	    }
-	  }
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
     let checkNull = squares.some(function (item) {
-		  return item === null;
-	  });
-	  return checkNull ? null : 'Draw'
+      return item === null;
+    });
+    return checkNull ? null : 'Draw'
   }
 
   //bestMove: lượt của Ai
 
   function bestMove(turn, squares) {
     let bestScore = -Infinity;
+    let depth = 0;
     const board = [...squares];
     let move;
-    for(let i=0; i<board.length; i++) {
-      if(board[i] === null) {
+    for (let i = 0; i < squares.length; i++) {
+      if (board[i] === null) {
         board[i] = ai;
-        // setSquares(squares);
-        setSquares(board);
-        let score = minimax(board, 0, false);
+        let score = minimax(board, depth, false);
         board[i] = null;
-        // setSquares(squares);
-        setSquares(board);
-        if(bestScore < score) {
+        if (score > bestScore) {
           bestScore = score;
           move = i;
+          // if (score === 10 && ) {
+          //   break;
+          // }
         }
       }
     }
@@ -76,67 +75,61 @@ export default function Demo() {
     setSquares(squares);
     setTurn(!turn);
   }
-  
+
   // status
 
   let winner = checkWinner(squares);
-  
+
   let status;
   if (winner === 'Draw') {
     status = winner;
-  } else if (winner){
+  } else if (winner) {
     status = "Winner: " + winner;
   } else {
     status = 'Turn : ' + human;
   }
 
   //minimax
-  
-function minimax(squares, depth, isMaximizing) {
-	let result = checkWinner(squares);
-	if (result !== null ) {
-		if (result === human) {
-      return -10;
-    } else if (result === ai) {
-      return 10;
-    } else if(result === 'Draw'){
-      return 0;
+
+  function minimax(squares, depth, isMaximizing) {
+    let result = checkWinner(squares);
+    // squares = JSON.parse(JSON.stringify(squares));
+    if (result !== null) {
+      if (result === human) {
+        return -10;
+      } else if (result === ai) {
+        return 10;
+      } else if (result === 'Draw') {
+        return 0;
+      }
     }
-	}
-	if (isMaximizing) {
-		let bestScore = -Infinity;
-		for( let i=0; i<squares.length; i++) {
-			if (squares[i] === null) {
-				squares[i] = ai;
-        setSquares(squares);
-				let score = minimax(squares, depth +1, false);
-				squares[i] = null;
-        setSquares(squares);
-				bestScore = Math.max(score, bestScore);
-			}
-		return bestScore;
-		} 	
-	} else {
-		let bestScore = Infinity;
-		for( let i=0; i<squares.length; i++) {
-			if (squares[i] === null) {
-				squares[i] = human;
-        setSquares(squares);
-				let score = minimax(squares, depth +1, true);
-				squares[i] = null;
-        setSquares(squares);
-				bestScore = Math.min(score, bestScore) 
-			}
-		}	
-		return bestScore;
-	}
+    if (isMaximizing) {
+      let bestScore = -Infinity;
+      for (let i = 0; i < squares.length; i++) {
+        if (squares[i] === null) {
+          squares[i] = ai;
+          let score = minimax(squares, depth + 1, false);
+          squares[i] = null;
+          bestScore = Math.max(score, bestScore);
+        }
 
-}
+      }
+      return bestScore;
+    } else {
+      let bestScore = +Infinity;
+      for (let i = 0; i < squares.length; i++) {
+        if (squares[i] === null) {
+          squares[i] = human;
+          let score = minimax(squares, depth + 1, true);
+          squares[i] = null;
+          bestScore = Math.min(score, bestScore)
+        }
+      }
+      return bestScore;
+    }
 
-//   } else {
-//     status = "Next player: " + (turn ? "O" : "X");
-//   }
-  
+  }
+
   return (
     <div className="game">
       <div className="game-board">
